49958d416544f88e84764c17a855cd3a27a3f339
waleedhassanien-info <waleedhassanien@gmail.com>
2022-04-07 12:27:16 +0500

    changes

--- contact_mail.php
+++ contact_mail.php
@@ -1,8 +1,8 @@
 <?php
 $toEmail = "waleedhassanien@gmail.com";
-$subject = $_POST["subject"];
-$mailHeaders = "From:". $_POST["userEmail"] .">\r\n";
-$message = "Name: ".$_POST["userName"].", \r\nEmail: ".$_POST["userEmail"].", \r\nPhone: ".$_POST["phone"].",  \r\nSubject: ".$_POST["subject"].", \r\nMessage: ".$_POST["message"];
+$subject = $_GET["subject"];
+$mailHeaders = "From:". $_GET["userEmail"] .">\r\n";
+$message = "Name: ".$_GET["userName"].", \r\nEmail: ".$_GET["userEmail"].", \r\nPhone: ".$_GET["phone"].",  \r\nSubject: ".$_GET["subject"].", \r\nMessage: ".$_GET["message"];
 if(mail($toEmail, $subject, $message, $mailHeaders)) {
 print "<p class='success'>Your message has been sent. Thank you!</p>";
 } else {
